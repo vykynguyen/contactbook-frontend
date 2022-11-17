589e1b011444e01a81ce06bb2d7be3231db68821
vykynguyen <kyb1910661@student.ctu.edu.vn>
2022-11-17 21:29:19 +0700

    Create a contact listing page

---
+++ src/router/index.js
@@ -0,0 +1,33 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import ContactBook from '@/views/ContactBook.vue';
+
+const routes = [
+    {
+        path: '/',
+        name: 'contactbook',
+        component: ContactBook,
+    },
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'notfound',
+        component: () => import('@/views/NotFound.vue'),
+    },
+    {
+        path: '/contacts/:id',
+        name: 'contact.edit',
+        component: () => import('@/views/ContactEdit.vue'),
+        props: (route) => ({ contactId: parseInt(route.params.id) })
+    },
+    {
+        path: '/contacts/add',
+        name: 'contact.add',
+        component: () => import('@/views/ContactAdd.vue'),
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes,
+});
+
+export default router;

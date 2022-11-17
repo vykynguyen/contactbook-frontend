608ca28355458d74b22fbc64cefead679421e4ba
vykynguyen <kyb1910661@student.ctu.edu.vn>
2022-11-17 21:42:56 +0700

    git project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppHeader from '@/components/AppHeader.vue';
+export default {
+  components: {
+    AppHeader,
+},
+};
+</script>
+
+<template>
+  <AppHeader />
+  <div class="container mt-3">
+    <router-view />
+  </div>
+</template>
+
+<style>
+.page {
+  max-width: 400px;
+  margin: auto;
+}
+</style>
